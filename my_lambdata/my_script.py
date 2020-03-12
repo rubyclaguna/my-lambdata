a9cc11739da2768e1badf990343385a6533d34b8
rubyclaguna <rubyclaguna@gmail.com>
2020-03-11 19:45:10 -0700

    import function from mod

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -2,7 +2,12 @@
 
 import pandas 
 
+from my_lambdata.my_mod import enlarge
+
 print("Happy Tuesday Night")
 
 df = pandas.DataFrame({"x":[1,2,3], "y": [4,5,6]})
 print(df.head())
+
+x = 5 
+print("ENLARGE", x, "TO", enlarge(x))
