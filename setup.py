4ab612524b6185be57be5e2b0ba9301bca2e42ad
rubyclaguna <rubyclaguna@gmail.com>
2020-03-11 20:14:23 -0700

    prepare to push to pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="my-lambdata",
+    version="1.0",
+    author="rubyclaguna",
+    author_email="rubyclaguna@gmail.com",
+    description="For example purposes",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/rubyclaguna/my-lambdata",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
